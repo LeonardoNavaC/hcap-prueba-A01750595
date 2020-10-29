6b1e11d7bae3ebc8df71e127bf2e1802cff5170c
LeonardoNavaC <A01750595@itesm.mx>
2020-10-29 13:05:45 -0600

    Cambia ejemplo1.py

--- ejemplo1.py
+++ ejemplo1.py
@@ -2,7 +2,9 @@ import cv2
 
 imagen_i = cv2.imread("imagen.jpg")
 imagen = cv2.cvtColor(imagen_i,cv2.COLOR_BGR2RGB)
+print(imagen.shape)
 imagen2 = cv2.cvtColor(imagen_i,cv2.COLOR_BGR2GRAY)
+print(imagen2.shape)
 
 
 imagen = cv2.resize(imagen,(256,256))
